6999a98ff963da0f55a1f5456e48a2f873955c70
itcast <hello@itcast.cn>
2023-05-02 10:44:57 +0800

    搭建content模块，建表，生成po类

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,20 @@
+package com.xuecheng.content.model.dto;
+
+/**
+ * ClassName: QueryCourseParamsDto
+ * Package: com.xuecheng.content.model.dto
+ * Description:
+ *
+ * @Author: XKing
+ * @Create: 2023/5/1 - 18:00
+ * @Version: 1.0
+ */
+public class QueryCourseParamsDto {
+    //审核状态
+    private String auditStatus;
+    //课程状态
+    private String courseName;
+
+    private String publishStatus;
+
+}
